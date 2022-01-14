e002262a71c3b91678eb656fa67aff7da4c86766
Tyler Butler <tylerbu@microsoft.com>
2022-01-14 14:12:57 -0800

    buiuld failure in debug pkg

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import nodePolyfills from 'rollup-plugin-polyfill-node'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  // build: {
+  //   commonjsOptions: { include: [] },
+  // },
+  plugins: [
+    vue(),
+    {
+      ...nodePolyfills(),
+      enforce: 'pre',
+      apply: 'build'
+    },
+  ],
 })
